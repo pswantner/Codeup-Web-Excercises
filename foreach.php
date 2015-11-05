9bc2ada68d083757ca2b0172fa76562ff6a7e430
William Swantner <Compater@Williams-MacBook-Pro.local>
2015-11-05 10:46:24 -0600

    final commit for foreach, it taught me how to itterate arrays while itterating different data types and learning hlw to deal with it

--- foreach.php
+++ foreach.php
@@ -8,21 +8,30 @@ array(1,2,3),
 3.14,
 "12 + 7",
  false,
-  (string) 11));
-]
+  (string) 11
+];
 
 
 foreach($things as $value){
 	if(is_scalar($value)){
 
-		echo "$value is a scalar dataype";
+		echo "$value is a scalar dataype" . PHP_EOL;
 	}else if(is_string($value)){
-		echo "$value is a string ol datatype";
+		echo "$value is a string ol datatype". PHP_EOL;
 	}else if(is_array($value)){
-		echo "$value is a sting array";
+		echo "$value is a sting array". PHP_EOL;
 	}else if(is_null($value)){
-		echo "$value is a nullguy not a nilgai";
+		echo "$value is a nullguy not a nilgai". PHP_EOL;
 	}
 
 
+}
+
+foreach($things as $value){
+	if(is_scalar($value)){
+		echo "$value". PHP_EOL;
+	}else if(is_array($value)){
+		foreach($value as $item)
+			echo "$item". PHP_EOL;
+	}
 }
